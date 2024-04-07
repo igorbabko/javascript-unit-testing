975ac29fc912af6da923cffe2c6bccf53b3be40a
Igor Babko <i.i.babko@gmail.com>
2024-04-07 20:12:02 +0300

    make constants not depend on functions module

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,4 @@
-import { generatePeriodSelectOptions } from './functions'
-import { IconName, PageName, type NavItem } from './types'
+import { IconName, PageName, type NavItem, type SelectOption } from './types'
 
 export const LOCAL_STORAGE_KEY = 'time-tracker'
 
@@ -31,3 +30,25 @@ export const PERIOD_SELECT_OPTIONS = generatePeriodSelectOptions()
 export const LOW_PERCENT = 33
 export const MEDIUM_PERCENT = 66
 export const HUNDRED_PERCENT = 100
+
+function generatePeriodSelectOptions(): SelectOption<number>[] {
+  const periodsInMinutes = [
+    15, 30, 45, 60, 90, 120, 150, 180, 210, 240, 270, 300, 330, 360, 390, 420, 450, 480
+  ]
+
+  return periodsInMinutes.map(
+    (periodInMinutes): SelectOption<number> => ({
+      value: periodInMinutes * SECONDS_IN_MINUTE,
+      label: generatePeriodSelectOptionsLabel(periodInMinutes)
+    })
+  )
+}
+
+function generatePeriodSelectOptionsLabel(periodInMinutes: number): string {
+  const hours = Math.floor(periodInMinutes / MINUTES_IN_HOUR)
+    .toString()
+    .padStart(2, '0')
+  const minutes = (periodInMinutes % MINUTES_IN_HOUR).toString().padStart(2, '0')
+
+  return `${hours}:${minutes}`
+}
