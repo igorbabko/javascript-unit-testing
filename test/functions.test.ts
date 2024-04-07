c647afdad9dced04366ea1c9285a021969e96c69
Igor Babko <i.i.babko@gmail.com>
2024-04-07 22:58:00 +0300

    .

--- test/functions.test.ts
+++ test/functions.test.ts
@@ -1,11 +1,7 @@
-import { expect, it } from 'vitest'
-import { formatSeconds, formatSecondsWithSign } from '../src/functions'
+import { expect, it, vi } from 'vitest'
+import { formatSeconds, formatSecondsWithSign, normalizeSelectValue, id } from '../src/functions'
 
 it('formats seconds', () => {
-  // const formattedSeconds = formatSeconds(0);
-
-  // console.log(formatSeconds(27 * 60 * 60))
-
   expect(formatSeconds(0)).toBe('00:00:00')
   expect(formatSeconds(60)).toBe('00:01:00')
   expect(formatSeconds(180)).toBe('00:03:00')
@@ -13,15 +9,9 @@ it('formats seconds', () => {
   expect(formatSeconds(30 * 60)).toBe('00:30:00')
   expect(formatSeconds(60 * 60)).toBe('01:00:00')
   expect(formatSeconds(24 * 60 * 60)).toBe('00:00:00')
-
-  // expect(2).toBe(2)
-  // expect(Math.sqrt(144)).toBe(12)
-  // expect(Math.sqrt(2)).toBe(Math.SQRT2)
 })
 
 it('formats seconds with sign', () => {
-  // vi.spyOn(formatSeconds).mockReturnValue('11111111')
-
   expect(formatSecondsWithSign(0)).toBe('+00:00:00')
   expect(formatSecondsWithSign(60)).toBe('+00:01:00')
   expect(formatSecondsWithSign(180)).toBe('+00:03:00')
@@ -37,6 +27,21 @@ it('formats seconds with sign', () => {
   expect(formatSecondsWithSign(-30 * 60)).toBe('-00:30:00')
   expect(formatSecondsWithSign(-60 * 60)).toBe('-01:00:00')
   expect(formatSecondsWithSign(-24 * 60 * 60)).toBe('-00:00:00')
+})
+
+it('normalizes select value', () => {
+  expect(normalizeSelectValue('random-string')).toBe('random-string')
+  expect(normalizeSelectValue(null)).toBe(null)
+  expect(normalizeSelectValue('1')).toBe(1)
+})
+
+it('generates random id', () => {
+  vi.spyOn(Date, 'now').mockReturnValueOnce(1)
+  vi.spyOn(Math, 'random').mockReturnValueOnce(10000)
+
+  expect(id()).toBe('1s')
+  // expect(id()).toBe('1s')
+  // expect(id()).toBe('1s')
 })
 
 // test('Squared', () => {
