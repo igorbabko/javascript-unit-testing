cf4b196326f54d86786e9259f27710b7698aa1c9
Igor Babko <i.i.babko@gmail.com>
2024-04-07 20:33:52 +0300

    wip

---
+++ test/functions.test.ts
@@ -0,0 +1,37 @@
+import { expect, it } from 'vitest'
+import { formatSeconds } from '../src/functions'
+
+it('formats seconds', () => {
+  // const formattedSeconds = formatSeconds(0);
+
+  // console.log(formatSeconds(27 * 60 * 60))
+
+  expect(formatSeconds(0)).toBe('00:00:00')
+  expect(formatSeconds(60)).toBe('00:01:00')
+  expect(formatSeconds(180)).toBe('00:03:00')
+  expect(formatSeconds(30 * 60)).toBe('00:30:00')
+  expect(formatSeconds(30 * 60)).toBe('00:30:00')
+  expect(formatSeconds(60 * 60)).toBe('01:00:00')
+  expect(formatSeconds(24 * 60 * 60)).toBe('00:00:00')
+
+  // expect(2).toBe(2)
+  // expect(Math.sqrt(144)).toBe(12)
+  // expect(Math.sqrt(2)).toBe(Math.SQRT2)
+})
+
+// test('Squared', () => {
+//   expect(squared(2)).toBe(4)
+//   expect(squared(12)).toBe(144)
+// })
+
+// test('JSON', () => {
+//   const input = {
+//     foo: 'hello',
+//     bar: 'world',
+//   }
+
+//   const output = JSON.stringify(input)
+
+//   expect(output).eq('{"foo":"hello","bar":"world"}')
+//   assert.deepEqual(JSON.parse(output), input, 'matches original')
+// })
