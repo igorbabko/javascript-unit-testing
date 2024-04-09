01fad6581cca34f80552a9f12a9df02a090fe788
Igor Babko <i.i.babko@gmail.com>
2024-04-09 11:52:05 +0300

    .

--- test/activities.test.ts
+++ test/activities.test.ts
@@ -1,6 +1,10 @@
-import { expect, it } from 'vitest'
-import { activities, initializeActivities } from '../src/activities'
+import { expect, it, beforeEach } from 'vitest'
+import { activities, createActivity, initializeActivities } from '../src/activities'
 import { Activity } from '../src/types'
+
+beforeEach(() => {
+  activities.value = []
+})
 
 it('initializes activities', () => {
   const codingActivity: Activity = {
@@ -25,4 +29,17 @@ it('initializes activities', () => {
 
   // expect(activities.value).toContainEqual(codingActivity)
   // expect(activities.value).toContainEqual(readingActivity)
+})
+
+it('creates activity', () => {
+  const codingActivity: Activity = {
+    id: '1',
+    name: 'Coding',
+    secondsToComplete: 7200
+  }
+
+  createActivity(codingActivity)
+
+  expect(activities.value).toHaveLength(1)
+  expect(activities.value).toContainEqual(codingActivity)
 })
