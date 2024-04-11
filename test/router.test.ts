83c502d2f4a64d0ad91e895f8a4052a8f9ee4da8
Igor Babko <i.i.babko@gmail.com>
2024-04-11 11:00:07 +0300

    normalizePageHash

---
+++ test/router.test.ts
@@ -0,0 +1,19 @@
+// @vitest-environment happy-dom
+
+import { expect, it } from 'vitest'
+import { normalizePageHash } from '../src/router'
+import { PageName } from '../src/types'
+
+it('normalizes valid page hash', () => {
+  Object.values(PageName).forEach((page) => {
+    window.location.hash = page
+
+    expect(normalizePageHash()).toBe(page)
+  })
+})
+
+it('normalizes invalid page hash', () => {
+  window.location.hash = 'invalid'
+
+  expect(normalizePageHash()).toBe(PageName.TIMELINE)
+})
