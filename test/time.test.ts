bcc864947b490533fca5b5eadacc60469b3865c4
Igor Babko <i.i.babko@gmail.com>
2024-04-12 22:11:06 +0300

    .

--- test/time.test.ts
+++ test/time.test.ts
@@ -1,6 +1,6 @@
 import { expect, it, vi } from 'vitest'
-import { endOfHour, isToday, toSeconds, today, tomorrow } from '../src/time'
 import { MILLISECONDS_IN_SECOND } from '../src/constants'
+import { endOfHour, isToday, toSeconds, today, tomorrow } from '../src/time'
 
 it('gets current date', () => {
   const dateA = new Date('1990-01-01')
@@ -30,29 +30,29 @@ it('gets current date', () => {
 
 it('gets date of tomorrow', () => {
   const dateA = new Date('1990-01-01')
-  const tomorrowOfDateA = new Date('1990-01-02')
+  const tomorrowA = new Date('1990-01-02')
 
   const dateB = new Date('2024-03-08')
-  const tomorrowOfDateB = new Date('2024-03-09')
+  const tomorrowB = new Date('2024-03-09')
 
   const dateC = new Date('2030-05-14')
-  const tomorrowOfDateC = new Date('2030-05-15')
+  const tomorrowC = new Date('2030-05-15')
 
   vi.setSystemTime(dateA)
 
-  expect(tomorrow()).toEqual(tomorrowOfDateA)
+  expect(tomorrow()).toEqual(tomorrowA)
 
   // await new Promise((resolve) => setTimeout(() => resolve(null), 1000))
 
   vi.setSystemTime(dateB)
 
-  expect(tomorrow()).toEqual(tomorrowOfDateB)
+  expect(tomorrow()).toEqual(tomorrowB)
 
   // await new Promise((resolve) => setTimeout(() => resolve(null), 1000))
 
   vi.setSystemTime(dateC)
 
-  expect(tomorrow()).toEqual(tomorrowOfDateC)
+  expect(tomorrow()).toEqual(tomorrowC)
 
   // await new Promise((resolve) => setTimeout(() => resolve(null), 1000))
 
@@ -61,17 +61,17 @@ it('gets date of tomorrow', () => {
 
 it('gets end of hour date', () => {
   const dateA = new Date('2024-04-10T10:15:00')
-  const endOfHourOfDateA = new Date('2024-04-10T11:00:00')
+  const endOfHourA = new Date('2024-04-10T11:00:00')
 
   const dateB = new Date('2024-04-10T20:00:00')
-  const endOfHourOfDateB = new Date('2024-04-10T21:00:00')
+  const endOfHourB = new Date('2024-04-10T21:00:00')
 
   const dateC = new Date('2024-04-10T12:59:00')
-  const endOfHourOfDateC = new Date('2024-04-10T13:00:00')
+  const endOfHourC = new Date('2024-04-10T13:00:00')
 
-  expect(endOfHour(dateA)).toEqual(endOfHourOfDateA)
-  expect(endOfHour(dateB)).toEqual(endOfHourOfDateB)
-  expect(endOfHour(dateC)).toEqual(endOfHourOfDateC)
+  expect(endOfHour(dateA)).toEqual(endOfHourA)
+  expect(endOfHour(dateB)).toEqual(endOfHourB)
+  expect(endOfHour(dateC)).toEqual(endOfHourC)
 })
 
 it('checks if passed date is today', () => {
@@ -87,6 +87,8 @@ it('checks if passed date is today', () => {
 
   expect(isToday(dateA)).toBe(false)
   expect(isToday(dateB)).toBe(true)
+
+  vi.useRealTimers()
 })
 
 it('converts milliseconds to seconds', () => {
