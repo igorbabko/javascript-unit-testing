0210d90d8e87c4ae67c3745b34f81bfd4fa72122
Igor Babko <i.i.babko@gmail.com>
2024-04-14 09:25:26 +0300

    14. Test resetTimelineItemActivities function

--- test/timeline-items.test.ts
+++ test/timeline-items.test.ts
@@ -1,6 +1,6 @@
-import { expect, it } from 'vitest'
-import { updateTimelineItem } from '../src/timeline-items'
-import { TimelineItem } from '../src/types'
+import { expect, it, vi } from 'vitest'
+import { resetTimelineItemActivities, updateTimelineItem } from '../src/timeline-items'
+import { Activity, Hour, TimelineItem } from '../src/types'
 
 it('updates timeline item', () => {
   const timelineItem: TimelineItem = {
@@ -22,5 +22,66 @@ it('updates timeline item', () => {
   expect(updatedTimelineItem).toEqual(updatedFields)
 })
 
-it.todo('resets timeline item activities')
+it('resets timeline item activities', () => {
+  const date = new Date('2024-04-10T02:00:00')
+
+  vi.setSystemTime(date)
+
+  const trainingActivity: Activity = {
+    id: '1',
+    name: 'Training',
+    secondsToComplete: 3600
+  }
+  const readingActivity: Activity = {
+    id: '2',
+    name: 'Reading',
+    secondsToComplete: 7200
+  }
+  const timelineItems: TimelineItem[] = [
+    {
+      hour: 1,
+      activityId: trainingActivity.id,
+      activitySeconds: 1800,
+      isActive: false
+    },
+    {
+      hour: date.getHours() as Hour,
+      activityId: trainingActivity.id,
+      activitySeconds: 3600,
+      isActive: false
+    },
+    {
+      hour: 3,
+      activityId: readingActivity.id,
+      activitySeconds: 3600,
+      isActive: true
+    }
+  ]
+
+  resetTimelineItemActivities(timelineItems, trainingActivity)
+
+  expect(timelineItems).toEqual([
+    {
+      hour: 1,
+      activityId: null,
+      activitySeconds: 0,
+      isActive: false
+    },
+    {
+      hour: date.getHours(),
+      activityId: null,
+      activitySeconds: 3600,
+      isActive: false
+    },
+    {
+      hour: 3,
+      activityId: readingActivity.id,
+      activitySeconds: 3600,
+      isActive: true
+    }
+  ])
+
+  vi.useRealTimers()
+})
+
 it.todo('calculates tracked activity seconds')
