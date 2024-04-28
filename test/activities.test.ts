d7d4b8d17ee952cb95bbe4a538b6ede46c0645fd
Igor Babko <i.i.babko@gmail.com>
2024-04-28 23:46:07 +0300

    19. Use "beforeEach" for activity tests

--- test/activities.test.ts
+++ test/activities.test.ts
@@ -1,7 +1,17 @@
-import { describe, expect, it } from 'vitest'
+import { beforeEach, describe, expect, it } from 'vitest'
 import { calculateActivityCompletionPercentage, updateActivity } from '../src/activities'
 import { HUNDRED_PERCENT, SECONDS_IN_HOUR } from '../src/constants'
 import { Activity } from '../src/types'
+
+let activity: Activity
+
+beforeEach(() => {
+  activity = {
+    id: '1',
+    name: 'Training',
+    secondsToComplete: SECONDS_IN_HOUR * 1
+  }
+})
 
 describe('updateActivity', () => {
   const updatedFields: Activity = {
@@ -11,35 +21,17 @@ describe('updateActivity', () => {
   }
 
   it('updates original activity', () => {
-    const activity: Activity = {
-      id: '1',
-      name: 'Training',
-      secondsToComplete: SECONDS_IN_HOUR * 1
-    }
-
     updateActivity(activity, updatedFields)
 
     expect(activity).toEqual(updatedFields)
   })
 
   it('returns updated activity', () => {
-    const activity: Activity = {
-      id: '1',
-      name: 'Training',
-      secondsToComplete: SECONDS_IN_HOUR * 1
-    }
-
     expect(updateActivity(activity, updatedFields)).toEqual(updatedFields)
   })
 })
 
 it('calculates activity completion percentage', () => {
-  const activity: Activity = {
-    id: '1',
-    name: 'Training',
-    secondsToComplete: SECONDS_IN_HOUR * 1
-  }
-
   expect(calculateActivityCompletionPercentage(activity, SECONDS_IN_HOUR * 0)).toBe(0)
   expect(calculateActivityCompletionPercentage(activity, SECONDS_IN_HOUR * 0.5)).toBe(50)
   expect(calculateActivityCompletionPercentage(activity, SECONDS_IN_HOUR * 1)).toBe(HUNDRED_PERCENT)
