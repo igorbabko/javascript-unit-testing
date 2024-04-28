7ae6195c364996b6f1b813992db69eb1e5870619
Igor Babko <i.i.babko@gmail.com>
2024-04-28 16:02:08 +0300

    . beforeEach

--- test/activities.test.ts
+++ test/activities.test.ts
@@ -1,14 +1,19 @@
-import { describe, expect, it } from 'vitest'
+import { beforeEach, describe, expect, it } from 'vitest'
 import { calculateActivityCompletionPercentage, updateActivity } from '../src/activities'
 import { HUNDRED_PERCENT, SECONDS_IN_HOUR } from '../src/constants'
 import { Activity } from '../src/types'
 
-describe('updateActivity', () => {
-  const activity = {
+let activity: Activity
+
+beforeEach(() => {
+  activity = {
     id: '1',
     name: 'Training',
     secondsToComplete: SECONDS_IN_HOUR * 1
   }
+})
+
+describe('updateActivity', () => {
   const updatedFields: Activity = {
     id: '2',
     name: 'Reading',
@@ -27,12 +32,6 @@ describe('updateActivity', () => {
 })
 
 it('calculates activity completion percentage', () => {
-  const activity: Activity = {
-    id: '1',
-    name: 'Training',
-    secondsToComplete: SECONDS_IN_HOUR * 1
-  }
-
   expect(calculateActivityCompletionPercentage(activity, SECONDS_IN_HOUR * 0)).toBe(0)
   expect(calculateActivityCompletionPercentage(activity, SECONDS_IN_HOUR * 0.5)).toBe(50)
   expect(calculateActivityCompletionPercentage(activity, SECONDS_IN_HOUR * 1)).toBe(HUNDRED_PERCENT)
