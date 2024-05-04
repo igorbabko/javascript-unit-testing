63ad7e086e8c03941471c187bd46369cdee3f9a3
Igor Babko <i.i.babko@gmail.com>
2024-05-04 21:45:45 +0300

    23. Use "test.each" for router and time module tests

--- test/time.test.ts
+++ test/time.test.ts
@@ -1,67 +1,37 @@
-import { expect, it, vi } from 'vitest'
+import { expect, it, test, vi } from 'vitest'
 import { MILLISECONDS_IN_SECOND } from '../src/constants'
 import { endOfHour, isToday, toSeconds, today, tomorrow } from '../src/time'
 
-it('gets current date', () => {
-  const dateA = new Date('1990-01-01')
-  const dateB = new Date('2024-03-08')
-  const dateC = new Date('2030-05-12')
+test.each([
+  [new Date('1990-01-01'), new Date('1990-01-01')],
+  [new Date('2024-03-08'), new Date('2024-03-08')],
+  [new Date('2030-05-12'), new Date('2030-05-12')]
+])('today(%o) -> %o', (date, expectedDate) => {
+  vi.setSystemTime(date)
 
-  vi.setSystemTime(dateA)
-
-  expect(today()).toEqual(dateA)
-
-  vi.setSystemTime(dateB)
-
-  expect(today()).toEqual(dateB)
-
-  vi.setSystemTime(dateC)
-
-  expect(today()).toEqual(dateC)
-
-  vi.useRealTimers()
-
-  // await new Promise((resolve) => setTimeout(() => resolve(null), 1000))
-})
-
-it('gets date of tomorrow', () => {
-  const dateA = new Date('1990-01-01')
-  const tomorrowA = new Date('1990-01-02')
-
-  const dateB = new Date('2024-03-08')
-  const tomorrowB = new Date('2024-03-09')
-
-  const dateC = new Date('2030-05-12')
-  const tomorrowC = new Date('2030-05-13')
-
-  vi.setSystemTime(dateA)
-
-  expect(tomorrow()).toEqual(tomorrowA)
-
-  vi.setSystemTime(dateB)
-
-  expect(tomorrow()).toEqual(tomorrowB)
-
-  vi.setSystemTime(dateC)
-
-  expect(tomorrow()).toEqual(tomorrowC)
+  expect(today()).toEqual(expectedDate)
 
   vi.useRealTimers()
 })
 
-it('gets end of hour date', () => {
-  const dateA = new Date('2024-04-10T10:15:00')
-  const endOfHourA = new Date('2024-04-10T11:00:00')
+test.each([
+  [new Date('1990-01-01'), new Date('1990-01-02')],
+  [new Date('2024-03-08'), new Date('2024-03-09')],
+  [new Date('2030-05-12'), new Date('2030-05-13')]
+])('tomorrow(%o) -> %o', (date, expectedDate) => {
+  vi.setSystemTime(date)
 
-  const dateB = new Date('2024-04-10T20:00:00')
-  const endOfHourB = new Date('2024-04-10T21:00:00')
+  expect(tomorrow()).toEqual(expectedDate)
 
-  const dateC = new Date('2024-04-10T12:59:00')
-  const endOfHourC = new Date('2024-04-10T13:00:00')
+  vi.useRealTimers()
+})
 
-  expect(endOfHour(dateA)).toEqual(endOfHourA)
-  expect(endOfHour(dateB)).toEqual(endOfHourB)
-  expect(endOfHour(dateC)).toEqual(endOfHourC)
+test.each([
+  [new Date('2024-04-10T10:15:00'), new Date('2024-04-10T11:00:00')],
+  [new Date('2024-04-10T20:00:00'), new Date('2024-04-10T21:00:00')],
+  [new Date('2024-04-10T12:59:00'), new Date('2024-04-10T13:00:00')]
+])('endOfHour(%o) -> %o', (date, expectedDate) => {
+  expect(endOfHour(date)).toEqual(expectedDate)
 })
 
 it('checks if passed date is today', () => {
@@ -73,18 +43,15 @@ it('checks if passed date is today', () => {
   expect(isToday(dateA)).toBe(true)
   expect(isToday(dateB)).toBe(false)
 
-  vi.setSystemTime(dateB)
-
-  expect(isToday(dateA)).toBe(false)
-  expect(isToday(dateB)).toBe(true)
-
   vi.useRealTimers()
 })
 
-it('converts milliseconds to seconds', () => {
-  expect(toSeconds(-MILLISECONDS_IN_SECOND * 10)).toBe(-10)
-  expect(toSeconds(-MILLISECONDS_IN_SECOND * 1)).toBe(-1)
-  expect(toSeconds(MILLISECONDS_IN_SECOND * 0)).toBe(0)
-  expect(toSeconds(MILLISECONDS_IN_SECOND * 1)).toBe(1)
-  expect(toSeconds(MILLISECONDS_IN_SECOND * 10)).toBe(10)
+test.each([
+  [-MILLISECONDS_IN_SECOND * 10, -10],
+  [-MILLISECONDS_IN_SECOND * 1, -1],
+  [MILLISECONDS_IN_SECOND * 0, 0],
+  [MILLISECONDS_IN_SECOND * 1, 1],
+  [MILLISECONDS_IN_SECOND * 10, 10]
+])('toSeconds(%i) -> %i', (milliseconds, seconds) => {
+  expect(toSeconds(milliseconds)).toBe(seconds)
 })
